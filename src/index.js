bbc64a57eb4427e5467fc99a4a3819d9785f72cb
louissarg <louissarg@gmail.com>
2024-10-15 21:13:06 +0200

    updated city name

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-function refreshWeather(response) {
-  let temperatureElement = response.data.temperature.current;
-  temperature.innerHTML = Math.round(temperature);
-}
-
 function searchCity(city) {
-  let apikey = "baef1db434f00t5c6510a0f45a35996o";
+  let apiKey = "baef1db434f00t5c6510a0f45a35996o";
   let apiUrl =
     "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
-  axios.get(apiUrl).then(refreshWeather);
+  console.log(apiUrl);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.Value;
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
