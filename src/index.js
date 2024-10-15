b48e7edeb85cf72e4935a97e3bf46da1e122b92a
louissarg <louissarg@gmail.com>
2024-10-15 21:20:11 +0200

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.Value;
+  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
